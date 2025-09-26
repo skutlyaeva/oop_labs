885c2f479036e74ee881d0ad099e4d2930713042
skutlyaeva <skutlyaeva@gmail.com>
2025-09-27 01:14:27 +0300

    Add icorrect input

--- laba1/src/main.cpp
+++ laba1/src/main.cpp
@@ -7,6 +7,10 @@ int main() {
     unsigned int n;
     std::cout << "Enter a positive number: ";
     std::cin >> n;
+    if (!std::cin) {
+        std::cout << "incorrect input" << std::endl;
+        return 1;
+    }
     std::cout << n << " -> " << func(n) << std::endl;
     
     return 0;
